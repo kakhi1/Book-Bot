2a7040b273c766acdecbb2403c8f6e8ed462ec4c
Kakhi Mtchedluri <kmchedluri@gmail.com>
2023-09-03 20:09:09 +0400

    changes in action.py  file

--- actions/actions.py
+++ actions/actions.py
@@ -18,6 +18,8 @@ import  openai
 
 load_dotenv(".env")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 
 class ActionBooksPrice(Action):
     def name(self) -> Text:
@@ -27,36 +29,28 @@ class ActionBooksPrice(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        print("Current slots:", tracker.current_slot_values())
-        print("Latest user message:", tracker.latest_message.get("text"))
         book_name = tracker.get_slot('book_name')
         print(f"Book name: {book_name}")
         base_url = "https://www.googleapis.com/books/v1/volumes"    
         params = {
             "q": f"intitle:{book_name}",
             "country": 'US',
-            "key": GOOGLE_API_KEY 
-,
+            "key": GOOGLE_API_KEY,
         }
     
         response = requests.get(base_url, params=params)
-        data = response.json()
-        # print(json.dumps(data, indent=4))
-        
+        data = response.json() 
 
         if "items" in data:
             book_info = data["items"][0]
             self.display_price_info(dispatcher, book_info)
         else:
-            dispatcher.utter_message("Book not found.")
+            dispatcher.utter_message("I'm sorry, but it seems like we don't have the book you're looking for.")
 
         return []
     def display_price_info(self, dispatcher: CollectingDispatcher, book_info):
-        sale_info = book_info.get("saleInfo", {})
-        print(f" sale_info : {sale_info }")
-        saleability = sale_info.get("saleability", {})
-        print(f" saleability: {saleability}")
-        
+        sale_info = book_info.get("saleInfo", {})       
+        saleability = sale_info.get("saleability", {})       
     
         if saleability == "FOR_SALE" and "retailPrice" in sale_info:
             retail_price = sale_info["retailPrice"]
@@ -64,7 +58,7 @@ class ActionBooksPrice(Action):
             currency = retail_price.get("currencyCode", "")
             price_message = f"Price: {price_amount} {currency}"
         else:
-            price_message = "Price: Not for sale."
+            price_message = "Not currently available for sale."
     
         dispatcher.utter_message(price_message)
 
@@ -88,7 +82,6 @@ class ActionBooksDescription(Action):
     
         response = requests.get(base_url, params=params)
         data = response.json()
-
         if "items" in data:
             book_info = data["items"][0]
             self.display_description(dispatcher, book_info)
@@ -110,7 +103,7 @@ class ActionGetBooksNamesByAuthor(Action):
         return "action_find_Books_by_author"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict):
-        # Set your OpenAI API key here
+        # OpenAI API 
         openai.api_key = OPENAI_API_KEY
 
         # Get the value of the 'author_name' slot
@@ -142,14 +135,15 @@ class ActionBooksLink(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("Current slots:", tracker.current_slot_values())
+        print("Latest user message:", tracker.latest_message.get("text"))
         book_name = tracker.get_slot('book_name')
-        base_url = "https://www.googleapis.com/books/v1/volumes"
-    
+        print(f"wignis saxeli:{book_name}")
+        base_url = "https://www.googleapis.com/books/v1/volumes"  
         params = {
             "q": f"intitle:{book_name}",
             "country": 'US',
-            "key": GOOGLE_API_KEY ,
-            
+            "key": GOOGLE_API_KEY ,          
         }
     
         response = requests.get(base_url, params=params)
